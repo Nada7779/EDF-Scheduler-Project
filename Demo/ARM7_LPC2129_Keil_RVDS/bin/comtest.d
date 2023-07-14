26c94ead52ee32e5932d99bb0ab92196ae89371b
Mohamed <128575094+msayed6789@users.noreply.github.com>
2023-07-14 19:14:57 +0300

    fff

--- Demo/ARM7_LPC2129_Keil_RVDS/bin/comtest.d
+++ Demo/ARM7_LPC2129_Keil_RVDS/bin/comtest.d
@@ -5,13 +5,3 @@
 .\bin\comtest.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\bin\comtest.o: .\FreeRTOSConfig.h
 .\bin\comtest.o: C:\Keil_v5\ARM\INC\Philips\lpc21xx.h
-.\bin\comtest.o: ..\..\Source\include\projdefs.h
-.\bin\comtest.o: ..\..\Source\include\portable.h
-.\bin\comtest.o: ..\..\Source\include\deprecated_definitions.h
-.\bin\comtest.o: ..\..\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
-.\bin\comtest.o: ..\..\Source\include\mpu_wrappers.h
-.\bin\comtest.o: ..\..\Source\include\task.h
-.\bin\comtest.o: ..\..\Source\include\list.h
-.\bin\comtest.o: ..\Common\include\serial.h
-.\bin\comtest.o: ..\Common\include\comtest.h
-.\bin\comtest.o: ..\Common\include\partest.h
